dc7636f0bbc47962d2d2c192b6552718e22b1d55
EduardoVazG <eduardo_vazg@outlook.com>
2024-05-24 09:44:41 -0600

    Primer comentario

---
+++ app/src/main/java/com/example/voxis/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.voxis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textViewRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.view_login);
+
+        textViewRegister = findViewById(R.id.registrarse);
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewRegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
