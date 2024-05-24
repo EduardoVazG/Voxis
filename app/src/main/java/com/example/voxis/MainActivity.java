1813fbfc22ad27236a33b6f514653ba405145cf8
Paola325 <paolitaperezlopez0@gmail.com>
2024-05-24 12:59:23 -0600

    base de datos

--- app/src/main/java/com/example/voxis/MainActivity.java
+++ app/src/main/java/com/example/voxis/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.voxis;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -28,5 +29,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 
 }
--- app/src/main/java/com/example/voxis/ViewRegistroActivity.java
+++ app/src/main/java/com/example/voxis/ViewRegistroActivity.java
@@ -1,19 +1,38 @@
 package com.example.voxis;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
 
 public class ViewRegistroActivity extends AppCompatActivity {
 
+    private EditText mNombre, mApellido, mUsuario, mCorreo, mContraseña;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.view_registro);
+
+        mNombre = findViewById(R.id.nombre);
+        mApellido = findViewById(R.id.apellido);
+        mUsuario = findViewById(R.id.usuario);
+        mCorreo = findViewById(R.id.email);
+        mContraseña = findViewById(R.id.password);
+
+
+    }
+
+    public void Registro(View view) {
+
+
+
     }
 }
