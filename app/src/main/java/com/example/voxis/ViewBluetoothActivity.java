d6f87fbab4a8b3f5436d1cc12d4f5552f3bf2341
EduardoVazG <eduardo_vazg@outlook.com>
2024-05-28 22:37:30 -0600

    Diseño

--- app/src/main/java/com/example/voxis/ViewBluetoothActivity.java
+++ app/src/main/java/com/example/voxis/ViewBluetoothActivity.java
@@ -3,18 +3,21 @@ package com.example.voxis;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
