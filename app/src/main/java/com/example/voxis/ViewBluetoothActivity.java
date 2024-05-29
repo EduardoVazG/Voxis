8840911cf2e5b3c19501ec4e9d16e34c672659c3
EduardoVazG <eduardo_vazg@outlook.com>
2024-05-28 22:21:56 -0600

    DActuallizacion de l Bluetooth

--- app/src/main/java/com/example/voxis/ViewBluetoothActivity.java
+++ app/src/main/java/com/example/voxis/ViewBluetoothActivity.java
@@ -5,6 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,42 +20,77 @@ import java.util.List;
 
 public class ViewBluetoothActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerViewContactos;
+    private SearchView searchView;
+    private ListView listViewContactos;
     private RecyclerView recyclerViewSeleccionados;
-    private AdaptadorContactos contactsAdapter;
     private AdaptadorContactos selectedContactsAdapter;
     private List<Contactos> contactosList2;
+    private List<Contactos> filteredContactosList;
     private List<Contactos> selectedContactosList;
+    private ArrayAdapter<String> listViewAdapter;
+    private List<String> contactosNamesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_bluetooth);
 
-        recyclerViewContactos = findViewById(R.id.recycler_view_contactos);
+        searchView = findViewById(R.id.search_view);
+        listViewContactos = findViewById(R.id.list_view_contactos);
         recyclerViewSeleccionados = findViewById(R.id.recycler_view_seleccionados);
+
         contactosList2 = new ArrayList<>();
+        filteredContactosList = new ArrayList<>();
         selectedContactosList = new ArrayList<>();
+        contactosNamesList = new ArrayList<>();
 
-        recyclerViewContactos.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewSeleccionados.setLayoutManager(new LinearLayoutManager(this));
-
-        contactsAdapter = new AdaptadorContactos(contactosList2, this, this::onContactSelected);
         selectedContactsAdapter = new AdaptadorContactos(selectedContactosList, this, null);
-
-        recyclerViewContactos.setAdapter(contactsAdapter);
         recyclerViewSeleccionados.setAdapter(selectedContactsAdapter);
+
+        listViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contactosNamesList);
+        listViewContactos.setAdapter(listViewAdapter);
+
+        listViewContactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Contactos selectedContact = filteredContactosList.get(position);
+                if (!selectedContactosList.contains(selectedContact)) {
+                    selectedContactosList.add(selectedContact);
+                    selectedContactsAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(ViewBluetoothActivity.this, "El contacto ya está seleccionado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterContactos(newText);
+                return false;
+            }
+        });
 
         obtenerContactosBD();
     }
 
-    private void onContactSelected(Contactos contacto) {
-        if (!selectedContactosList.contains(contacto)) {
-            selectedContactosList.add(contacto);
-            selectedContactsAdapter.notifyDataSetChanged();
-        } else {
-            Toast.makeText(this, "El contacto ya está seleccionado", Toast.LENGTH_SHORT).show();
+    private void filterContactos(String query) {
+        filteredContactosList.clear();
+        contactosNamesList.clear();
+
+        for (Contactos contacto : contactosList2) {
+            if (contacto.getName().toLowerCase().contains(query.toLowerCase())) {
+                filteredContactosList.add(contacto);
+                contactosNamesList.add(contacto.getName());
+            }
         }
+        listViewAdapter.notifyDataSetChanged();
     }
 
     private void obtenerContactosBD() {
@@ -74,6 +113,7 @@ public class ViewBluetoothActivity extends AppCompatActivity {
                     String hora = "12:00 PM"; // Hora por defecto
 
                     contactosList2.add(new Contactos(id, nombre, hora, R.drawable.perfil, telefono, correo, categoria));
+                    contactosNamesList.add(nombre);
                 } while (cursor.moveToNext());
             }
         } catch (Exception e) {
@@ -85,7 +125,8 @@ public class ViewBluetoothActivity extends AppCompatActivity {
             db.close();
         }
 
-        contactsAdapter.notifyDataSetChanged();
+        filteredContactosList.addAll(contactosList2);
+        listViewAdapter.notifyDataSetChanged();
     }
 
     public void Compartir(View view) {
